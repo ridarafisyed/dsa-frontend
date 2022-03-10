f9f31f0a26082992887e3a640ef33cacdb81dc5e
Rida R Syed <ridarafisyed@yahoo.com>
2022-03-10 20:11:19 +0500

    updating app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 /** @format */
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import logo from "./logo.svg";
-import "./App.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./hocs/Theme";
+
+import Layout from "./hocs/Layout";
+import Home from "./containers/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1> New aap working fine</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   );
 }
 
