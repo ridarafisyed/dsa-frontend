4d7454aac0dbd254e9f9932d7fc27245055a2175
Rida R Syed <ridarafisyed@yahoo.com>
2022-04-03 13:20:29 +0500

    update the create post page

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import Dashbboard from "./components/Dashboard/Dashbboard";
 import PrivateRoutes from "./utils/PrivateRoute";
 import PhotoGallery from "./components/Gallery/PhotoGallery";
 import BlogList from "./components/Blogs/BlogList";
-import CreatePost from "./components/Blogs/CreatePost";
 
 function App() {
   return (
@@ -24,11 +23,11 @@ function App() {
           <Layout>
             <Switch>
               <Route path="/login" component={Login} />
-              <Route exact path="/" component={CreatePost} />
+              <Route exact path="/" component={Home} />
               <Route path="/detailPost" component={Post} />
               <Route path="/gallery" component={PhotoGallery} />
               <Route path="/blogs" component={BlogList} />
-
+              <Route exact path="/create-post" component={CreatePost} />
               <PrivateRoutes path="/dashboard" component={Dashbboard} />
             </Switch>
           </Layout>
